18be11aefbf2d03f553f7ae350a666810dfcc60b
Peter-Varga08 <varga.peter826@gmail.com>
2021-06-09 22:34:14 +0200

    plots extracted

--- pre_processing.py
+++ pre_processing.py
@@ -3,11 +3,15 @@ import re
 import unicodedata
 
 
-def unicode_to_ascii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )
+def split_names(name):
+    names = None
+    if ' and ' in name:
+        names = name.split(' and ')
+    elif ' & ' in name:
+        names = name.split(' & ')
+    elif '\r\n' in name:
+        names = name.split('\r\n')
+    return names
 
 data = pd.read_csv('data/wiki_movie_plots_deduped.csv')
 print("Amount of movies in entire dataset:", len(data))
@@ -25,13 +29,7 @@ total_names = {}
 for cast in cast_per_movie_split:
     for name in cast:
         name = name.strip()
-        names = None
-        if 'and' in name:
-            names = name.split('and')
-        elif '&' in name:
-            names = name.split('&')
-        elif '\r\n' in name:
-            names = name.split('\r\n')
+        names = split_names(name)
         if names:  # e.g. 'Marlon Brando and Charlie Chaplin' or 'Marlon Brando & Charlie Chaplin'
             for name in names:
                 name = name.strip()
@@ -46,7 +44,7 @@ for cast in cast_per_movie_split:
                 total_names[name] += 1
 
 # Filter out invalid names, e.g. single letters or empty strings due to wrongly formatted data
-total_names = {name: count for name, count in total_names.items() if len(name.split()) > 1}
+total_names = {name: count for name, count in total_names.items() if len(name.strip()) > 3}
 total_names_list = list(total_names.keys())
 
 # Sort persons based on movie count
@@ -59,19 +57,22 @@ for key in sorted_keys:
 # Sanity check: no invalid name should occur
 print("Top 15 names:", list(sorted_names.keys())[0:15])
 
-# Get 100 labels by retrieving the top 100
-labels = list(sorted_names.keys())[0:100]
+# Get 10 labels by retrieving the top 10
+labels = list(sorted_names.keys())[0:10]
 
-# Get movie plots that correspond to an actor/actress from top1-100
-plots = []
-for label in labels:
-    curr_label = []
-    for i in range(1, len(data)):
-        if label in data.iloc[i]['Cast']:
-            curr_label.append(data.iloc[i]['Plot'])
-    plots.append(curr_label)
-
-
-
-
-
+# Get movie plots that correspond to an actor/actress from top10
+plots = {name: [] for name in total_names_list}
+for i in range(len(data)):
+    cast = data.iloc[i]['Cast']
+    cast = cast.split(',')
+    for name in cast:
+        name = name.strip()
+        names = split_names(name)
+        if names:
+            for name in names:
+                name = name.strip()
+                if len(name) > 3:
+                    plots[name].append(data.iloc[i]['Plot'])
+        else:
+            if len(name) > 3:
+                plots[name].append(data.iloc[i]['Plot'])
