fb83131df6f099e16b6ca1af470cb1abd8b3c991
Peter-Varga08 <varga.peter826@gmail.com>
2021-06-10 11:38:41 +0200

    train/valid/test split

--- pre_processing.py
+++ pre_processing.py
@@ -1,3 +1,4 @@
+import csv
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -11,6 +12,7 @@ def split_names(name):
     elif '\r\n' in name:
         names = name.split('\r\n')
     return names
+
 
 data = pd.read_csv('data/wiki_movie_plots_deduped.csv')
 print("Amount of movies in entire dataset:", len(data))
@@ -42,11 +44,11 @@ for cast in cast_per_movie_split:
             else:
                 total_names[name] += 1
 
-# Filter out invalid names, e.g. single letters or empty strings due to wrongly formatted data
+# 2) Filter out invalid names, e.g. single letters or empty strings due to wrongly formatted original data
 total_names = {name: count for name, count in total_names.items() if len(name.strip()) > 3}
 total_names_list = list(total_names.keys())
 
-# Sort persons based on movie count
+# 3) Sort persons based on movie count
 print("Sorting names based on movie appearances...")
 sorted_names = {}
 sorted_keys = sorted(total_names, key=total_names.get, reverse=True)
@@ -56,10 +58,7 @@ for key in sorted_keys:
 # Sanity check: no invalid name should occur
 print("Top 15 names:", list(sorted_names.keys())[0:15])
 
-# Get 10 labels by retrieving the top 10
-labels = list(sorted_names.keys())[0:10]
-
-# Get movie plots that correspond to an actor/actress from top10
+# 4) Get all movie plots
 plots = {name: [] for name in total_names_list}
 for i in range(len(data)):
     cast = data.iloc[i]['Cast']
@@ -76,8 +75,9 @@ for i in range(len(data)):
             if len(name) > 3:
                 plots[name].append(data.iloc[i]['Plot'])
 
-# Filter plots shorter than 1000 characters
-MIN_LENGTH = 1000
+# 5) Get 10 labels by retrieving the top 10
+labels = list(sorted_names.keys())[0:10]
+# 6) Get max plot length, subsequently used for filtering
 plot_lengths = {name: [len(plot) for plot in plots[name]] for name in labels}
 max_length = 0
 for name, lengths in plot_lengths.items():
@@ -85,17 +85,46 @@ for name, lengths in plot_lengths.items():
         if length > max_length:
             max_length = length
 
-# Filtering and Augmenting data:
-# - Discard each plot shorther than MIN_LENGTH
+# 7) Filtering and Augmenting data:
+# - Discard each plot shorter than MIN_LENGTH
 # - Split up the plots to subplots based on MIN_LENGTH
+MIN_LENGTH = 500
 plots_filtered = {label: [] for label in labels}
 for name in labels:
     for idx, plot in enumerate(plots[name]):
         if plot_lengths[name][idx] > MIN_LENGTH:
-            for j in range(max_length//MIN_LENGTH):
-                if len(plot[0 + j*1000: 1000*(j+1)]) == MIN_LENGTH:
-                    plots_filtered[name].append(plot[0 + j*1000: 1000*(j+1)])
+            for j in range(max_length//MIN_LENGTH):  # number of slices available within current plot
+                if len(plot[0 + j*MIN_LENGTH: MIN_LENGTH*(j+1)]) == MIN_LENGTH:
+                    plots_filtered[name].append(plot[0 + j*MIN_LENGTH: MIN_LENGTH*(j+1)])
                 # else:
                 #     plots_filtered[name].append(plot[0 + j*1000:])
                 #     break
 
+
+# 8) Train/Valid/Test split
+X = []
+y = []
+for idx, name in enumerate(plots_filtered):
+    for plot in plots_filtered[name]:
+        X.append(plot)
+        y.append(idx)
+
+# 8/A) Split data to train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# 8/B) Split data to train and validation sets
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+with open('plots_train.csv', 'w') as f:
+    train_writer = csv.writer(f, delimiter=',')
+    for x, y in zip(X_train, y_train):
+        train_writer.writerow([y, x])
+
+with open('plots_valid.csv', 'w') as f:
+    train_writer = csv.writer(f, delimiter=',')
+    for x, y in zip(X_val, y_val):
+        train_writer.writerow([y, x])
+
+with open('plots_test.csv', 'w') as f:
+    train_writer = csv.writer(f, delimiter=',')
+    for x, y in zip(X_test, y_test):
+        train_writer.writerow([y, x])
